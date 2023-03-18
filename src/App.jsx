abe432041e11887dc0a0751e57b1f8eb5eac41df
Hachikoi-the-creator <facka72@gmail.com>
2023-03-17 19:46:43 -0500

    added input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
-   return (
-    <div className="App">
+import { useState } from "react";
 
+function App() {
+  const [input, setInput] = useState(second);
+
+  return (
+    <div className="App">
+      <h1>App title</h1>
+      <div className="input-section">
+        <label>
+          Enter your phrase
+          <input type="text" />
+        </label>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
