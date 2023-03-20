06782462729f607220b8d6908177e68acefe0016
KazTraZ <kaztraz@gmail.com>
2023-03-20 14:04:58 +0100

    Promt text changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { Configuration, OpenAIApi } from "openai";
 
 export default function App() {
-  const getQuote = async () => {
-    const configuration = new Configuration({
-      apiKey: import.meta.env.VITE_CHAT_KEY,
-    });
+    const getQuote = async() => {
+        const configuration = new Configuration({
+            apiKey: import.meta.env.VITE_CHAT_KEY,
+        });
 
-    const openai = new OpenAIApi(configuration);
+        const openai = new OpenAIApi(configuration);
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-001",
-      prompt: "Give an inspiration quote from lebron james",
-      temperature: 0.4,
-      max_tokens: 64,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
+        const response = await openai.createCompletion({
+            model: "text-davinci-001",
+            prompt: "Give an inspiration quote from a famous person please",
+            temperature: 0.8,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        // TODO Generate the display of text coming back from the api call
+        console.log(response.data.choices[0].text);
+    };
 
-    console.log(response.data.choices[0].text);
-  };
-
-  return (
-    <div className="App">
-      <button onClick={getQuote}>Click me</button>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        button onClick = { getQuote } > Click me < /button> < /
+        div >
+    );
 }
