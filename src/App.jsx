c5b3111e63e8208d9221d452990d2ca0f7e53c1d
Zak Franczak <zakfranczak@gmail.com>
2023-03-21 21:44:03 -0500

    Button Style, Add unsplash api key

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ export default function App() {
     const openai = new OpenAIApi(configuration);
 
     const response = await openai.createCompletion({
-      model: "text-davinci-001",
-      prompt: "Give an inspiration quote from lebron james",
+      model: "text-davinci-003",
+      prompt: "Give an inspiration quote from not lebron james",
       temperature: 0.4,
       max_tokens: 64,
       top_p: 1,
@@ -19,12 +19,26 @@ export default function App() {
       presence_penalty: 0,
     });
 
-    console.log(response.data.choices[0].text);
+    const quote = response.data.choices[0].text;
+
+    console.log(quote);
+
+  //  function displ
+  };
+
+  const getImage = async (str) => {
+    const res = await axios(
+      `https://api.unsplash.com/search/photos?page=1&query=office&client_id=${
+        import.meta.env.VITE_SPLASH_AKEY
+      }`
+    );
+    console.log(res.data);
   };
 
   return (
     <div className="App">
-      <button onClick={getQuote}>Click me</button>
+      <button className='quoteButton' onClick={getQuote}>Get Quote</button>
+      <button className='imgButton' onClick={getImage}>Get Image</button>
     </div>
   );
 }
