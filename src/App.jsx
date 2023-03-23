41f1dce070d31368c7ec593f05a929f76ac7d340
Hachikoi-the-creator <facka72@gmail.com>
2023-03-23 13:45:03 -0500

    moved some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,33 @@
-import axios from "axios";
-import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 import Header from "./components/Header";
+import { getQuote } from "./utils/fetchData";
+import famousList from "./utils/famousList";
 
 export default function App() {
-  const [quote, setQuote] = useState("");
+  const [quote, setQuote] = useState({ text: "", author: "" });
   const [imageUrl, setImageUrl] = useState("");
-  const getQuote = async () => {
-    const configuration = new Configuration({
-      apiKey: import.meta.env.VITE_CHAT_KEY,
-    });
 
-    const openai = new OpenAIApi(configuration);
-
-    const response = await openai.createCompletion({
-      // What dataset/model to use via the api, there are more options if we go for hugginface i think... maybe check it out.
-      model: "text-davinci-001",
-      //The promt given to chatgpt the better the promt the better the responce should be
-      prompt: "Give an inspiration quote from a famous person please",
-      // Temp is from 0.0 - 2.0 and you can think of it as how crazy should the text be, from moderately to craycray
-      temperature: 0.8,
-      max_tokens: 64,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    // Start of image fetching thingy why the slash g?
-    const prompt = response.data.choices[0].text.replace(/[^a-zA-Z ]/g, "");
-    const unsplashUrl = `https://api.unsplash.com/photos/random?client_id=${
-      import.meta.env.VITE_SPLASH_AKEY
-    }&query=${prompt}`;
-
-    fetch(unsplashUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setImageUrl(data.urls.regular);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
-    setQuote(response.data.choices[0].text);
+  const generateHandler = async () => {
+    // * Add a new function in utils to randomize the name
+    const res = await getQuote(famousList[0]);
+    console.log("res??", res);
+    // first item is the quote, second could be the author or nothing
+    setQuote({ text: res[0], author: res[1] });
   };
 
   return (
     <>
       <Header />
       <div className="App">
-        <button onClick={getQuote}> Generate Quote </button>{" "}
-        {quote && imageUrl && (
+        <button onClick={generateHandler}> Generate Quote </button>{" "}
+        {/* {quote && imageUrl && (
           <div className="quote-container">
             <img src={imageUrl} alt="Unsplash" />
             <p className="quote"> {quote} </p>
           </div>
-        )}{" "}
+        )} */}
+        <p className="quote">{quote.text || "no quote"}</p>
+        <p className="author">{quote.author || "anonymous"}</p>
       </div>
     </>
   );
