85dc2c6b7234255e1f1087b83465894df45b9dc6
Zak Franczak <zakfranczak@gmail.com>
2023-03-24 08:55:28 -0500

    Image now represents athlete's sport

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ export default function App() {
   const generateHandler = async () => {
     // * Add a new function in utils to randomize the name
     const randomIndex = Math.floor(Math.random() * famousList.length);
-    const randomFamous = famousList[randomIndex];
+    const randomFamous = famousList[randomIndex].id;
+    const randomFamousImg = famousList[randomIndex].sport;
     const res = await getQuote();
     setQuote(res[0]);
     setAthlete(randomFamous);
 
     // first item is the quote, second could be the author or nothing
-    const inspireImg = await getImage(randomFamous);
+    const inspireImg = await getImage(randomFamousImg);
     setImageUrl(inspireImg);
   };
 
