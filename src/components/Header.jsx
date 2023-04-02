258fb1a20e61f6aecf99f2eec817034d4e89c3bd
Zak Franczak <zakfranczak@gmail.com>
2023-04-02 03:38:52 -0500

    fixed minor bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 // import PropTypes from "prop-types"
 
 import "../styles/header.scss";
-import FontAwesome from 'react-fontawesome'
 
 
 const Header = ({ title }) => {
@@ -12,7 +11,6 @@ const Header = ({ title }) => {
         <li className="navItem"><img src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"></img></li>
         <li className="navItem"><img src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"></img></li>
       </ul>
-      <i class="fa-regular fa-user"></i>
       <h1 className="title">{title}</h1>
       <h2>Get inspired with Inspirebot 3000: your personal motivational coach. With our AI-generated quotes and stunning photos, you'll have the inspiration you need to tackle any challenge.</h2>
     </header>
