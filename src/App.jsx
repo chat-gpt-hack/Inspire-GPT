323dc165d77bec41961625ed6edeaa2c9eb65254
Zak Franczak <zakfranczak@gmail.com>
2023-04-03 08:49:11 -0500

    Added footer and React-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { getImagesArr, getQuote } from "./utils/fetchData";
 import famousList from "./utils/famousList";
 import ImageOptions from "./components/ImageOptions";
@@ -50,6 +51,7 @@ export default function App() {
 
       {/* <ImgCanvas imageSrc={currImage} text={quote} /> */}
       <button onClick={generateHandler}> Generate Quote </button>
+      <Footer />
     </main>
   );
 }
