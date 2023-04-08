aab75fb9410e4e5f4c812be31f17a0430318eb22
KazTraZ <kaztraz@gmail.com>
2023-04-08 23:53:51 +0200

    Carousel semi functional alt images not rdy

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,25 +4,32 @@ export default function Carousel(props) {
   const { currImage, nextImage, prevImage, selectHandler, quote, athlete } = props;
 
 return (
-    <section className="carousel">
-
-      <div className='rowImg'>
+  <section className="carousel">
+      <div className="rowImg">
         <img
-            src={prevImage}
-            alt="missing img"
-            onClick={() => selectHandler(prevImage)}
+          className="preview"
+          src={prevImage}
+          alt="missing img"
+          onClick={() => selectHandler(prevImage)}
         />
-        <img
+        <div className="currImage-container">
+          <img
             src={currImage}
             alt="missing img"
             onClick={() => selectHandler(currImage)}
-        />
+          />
+          <div className="quote-container">
+            <p className="quote">{quote || "no quote"}</p>
+            <p className="author">{athlete || "no athlete"}</p>
+          </div>
+        </div>
         <img
-            src={nextImage}
-            alt="missing img"
-            onClick={() => selectHandler(nextImage)}
+          className="preview"
+          src={nextImage}
+          alt="missing img"
+          onClick={() => selectHandler(nextImage)}
         />
-      
+      </div>
       <article>
         <button onClick={prevImage} className="prev-btn">
           <FaChevronLeft style={{ fontSize: "500%" }} />
@@ -31,12 +38,6 @@ return (
           <FaChevronRight style={{ fontSize: "500%" }} />
         </button>
       </article>
-
-      <div className="quote-container">
-          <p className="quote">{quote || "no quote"}</p>
-          <p className="author">{athlete || "no athlete"}</p>
-        </div>
-      </div>
     </section>
   );
 }
