8ff0c21e24f7d679c97a9bc3bcfd9c8d7ac82167
KazTraZ <kaztraz@gmail.com>
2023-04-08 23:38:41 +0200

    Footer fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import "../styles/footer.scss";
-import { FaGithub, FaReact, FaLinkedin } from "react-icons/fa"
+import { FaGithub, FaReact, FaLinkedin, FaGlobeEurope, FaGlobeAmericas } from "react-icons/fa"
 
 const Footer = () => {
   return (
@@ -7,21 +7,21 @@ const Footer = () => {
       <ul className="footerList">
         <ul className="author">
           <li className="footItem">Adan Moreno Flores</li>
-          <li className="footItem"><FaGithub className="brandIcon" /></li>
-          <li className="footItem"><FaReact className="brandIcon" /></li>
-          <li className="footItem"><FaLinkedin className="brandIcon" /></li>
+          <li className="footItem"><a href="https://github.com/Hachikoi-the-creator"><FaGithub className="brandIcon" /></a></li>
+          <li className="footItem"><a href="https://solid-portfolio-pi.vercel.app/"><FaGlobeEurope className="brandIcon" /></a></li>
+          <li className="footItem"><a href="https://www.linkedin.com/in/adan-moreno7/"><FaLinkedin className="brandIcon" /></a></li>
         </ul>
         <ul className="author">
           <li className="footItem">Josef Dan Karlsson</li>
-          <li className="footItem"><FaGithub className="brandIcon" /></li>
-          <li className="footItem"><FaReact className="brandIcon" /></li>
-          <li className="footItem"><FaLinkedin className="brandIcon" /></li>
+          <li className="footItem"><a href="https://github.com/kaztraz"><FaGithub className="brandIcon" /></a></li>
+          <li className="footItem"><a href="http://www.jdkarlsson.com/"><FaGlobeEurope className="brandIcon" /></a></li>
+          <li className="footItem"><a href="https://www.linkedin.com/in/jdkarlsson/"><FaLinkedin className="brandIcon" /></a></li>
         </ul>
         <ul className="author">
           <li className="footItem">Zak Franczak</li>
-          <li className="footItem"><FaGithub className="brandIcon" /></li>
-          <li className="footItem"><FaReact className="brandIcon" /></li>
-          <li className="footItem"><FaLinkedin className="brandIcon" /></li>
+          <li className="footItem"><a href="https://github.com/zfranczak"><FaGithub className="brandIcon" /></a></li>
+          <li className="footItem"><a href="https://zfranczak.github.io/portfolio/"><FaGlobeAmericas className="brandIcon"/></a></li>
+          <li className="footItem"><a href="https://www.linkedin.com/in/zachary-franczak/"><FaLinkedin className="brandIcon" /></a></li>
         </ul>
       </ul>
       <div className="logo-container">
