ccf14a6946c0a2d642db5d1bdbac5a7e67fab401
Zak Franczak <zakfranczak@gmail.com>
2023-04-10 12:17:09 -0500

    loader style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,6 +3,7 @@ import '../styles/loader.scss';
 const Loader = () => {
   return (
     <div className="loader">
+      <h2 className='icon'>InspireBOT<a className='accent'>3000</a></h2>
       <div className="loader-inner">
         <div className="loader-line-wrap">
           <div className="loader-line"></div>
